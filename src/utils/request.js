512b1bcd27a62dcf7373c94b3159fd98cf5ffe81
John <johntang1026@gmail.com>
2019-05-16 15:04:59 +0930

    tweak

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,20 +1,15 @@
 import fetch from 'dva/fetch';
-import { config } from '../config';
-
-function parseJSON(response) {
-  return response.json();
-}
 
 function checkStatus(response) {
   if (response.status >= 200 && response.status < 300) {
     return response;
   }
+
   const error = new Error(response.statusText);
   error.response = response;
-  throw error;  
+  throw error;
 }
 
-// function showError
 /**
  * Requests a URL, returning a promise.
  *
@@ -22,12 +17,18 @@ function checkStatus(response) {
  * @param  {object} [options] The options we want to pass to "fetch"
  * @return {object}           An object containing either "data" or "err"
  */
-export default function request(uri, options) {
-  var url = config.url.API_URL + uri
-  console.log(url)
-  return fetch(url, options)
-    .then(checkStatus)
-    .then(parseJSON)
-    .then( data => data)
-    .catch( err => err);
+async function request(url, options) {
+  const response = await fetch(url, options);
+
+  checkStatus(response);
+
+  const data = await response.json();
+
+  const res = {
+    data,
+    headers: response.headers,
+  };
+  return res;
 }
+
+export default request;
